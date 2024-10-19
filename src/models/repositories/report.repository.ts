5eb3c879d6298220c8cc87db733ff2f7783dc8a2
TRINHDINHDAI2306 <trinhdinhdai2002@gmail.com>
2024-10-19 23:26:03 +0700

    REPORT: Coding controller/module/service/dto Report

---
+++ src/models/repositories/report.repository.ts
@@ -0,0 +1,6 @@
+import { EntityRepository, Repository } from 'typeorm'
+
+import { Report } from '../entities/report.entity'
+
+@EntityRepository(Report)
+export class ReportRepository extends Repository<Report> {}
