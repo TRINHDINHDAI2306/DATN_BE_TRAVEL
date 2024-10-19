42256f359da74ef146e732fc73b69189b4b1c31d
TRINHDINHDAI2306 <trinhdinhdai2002@gmail.com>
2024-10-19 23:18:14 +0700

    PROVINCE: Coding controller/module/service/dto province

---
+++ src/modules/province/province.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+
+import { ProvinceController } from './province.controller'
+import { ProvinceService } from './province.service'
+
+@Module({
+  imports: [],
+  providers: [ProvinceService],
+  controllers: [ProvinceController],
+  exports: [],
+})
+export class ProvinceModule {}
